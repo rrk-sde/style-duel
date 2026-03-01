cb39076ee6a4c1ecbdeb1be425413e0e0d18322b
Rajeev <Rajeevrk9901@gmail.com>
2026-03-01 06:03:36 +0530

    add build scripts and finalize week 1 mvp

---
+++ dist/assets/content.tsx-loader-78M1J269.js
@@ -0,0 +1,13 @@
+(function () {
+  'use strict';
+
+  const injectTime = performance.now();
+  (async () => {
+    const { onExecute } = await import(
+      /* @vite-ignore */
+      chrome.runtime.getURL("assets/content.tsx-982aG8EP.js")
+    );
+    onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
+  })().catch(console.error);
+
+})();
